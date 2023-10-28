ae2ce9faa84ca4739bd173cebcab04661a593d3d
Cruizzer <archcruizer@gmail.com>
2023-10-29 00:18:17 +0100

    Added deletion functionality

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -26,12 +26,36 @@ def calculator():
 
 @app.route('/income', methods=['GET', 'POST'])
 def addIncome():
+    income = None
     form = AddIncomeExpenditureForm()
     if form.validate_on_submit():
-        flash('Yay, added income')
+        if request.method == 'POST':
+            name = request.form['name']
+            amount = request.form['amount']
+
+            income = models.Income.query.filter_by(name=name).first()
+            
+            try:
+                if income:
+                    flash('Income with that name already exists!', 'danger')
+                else:
+                    flash('Successfully added income!', 'success')
+                    income = models.Income(name=name, amount=amount)
+                    db.session.add(income)
+                    db.session.commit()
+                
+                return redirect(url_for('addIncome'))
+
+            except:
+                return "There was an error adding the income!"
+        
+    else:
+        income = models.Income.query.order_by(models.Income.date_created)
+
     return render_template('create.html',
                             title='Income',
-                            form=form)
+                            form=form,
+                            income=income)
 
 @app.route('/expenditure', methods=['GET', 'POST'])
 def addExpenditure():
@@ -65,3 +89,54 @@ def addExpenditure():
                             title='Expenditure',
                             form=form,
                             expenditure=expenditure)
+
+
+@app.route('/<entry_type>/update/<int:id>', methods=['GET', 'POST'])
+def update_entry(entry_type, id):
+    form = AddIncomeExpenditureForm()
+    if entry_type == 'income':
+        entry = models.Income.query.get_or_404(id)
+    elif entry_type == 'expenditure':
+        entry = models.Expenditure.query.get_or_404(id)
+    else:
+        abort(404)  # handle other cases as needed
+
+    if form.validate_on_submit():
+        name = request.form['name']
+        amount = request.form['amount']
+
+        try:
+            entry.name = name
+            entry.amount = amount
+            db.session.commit()
+            flash('Successfully updated entry!', 'success')
+            # return redirect(url_for('add' + entry_type.capitalize()))
+            return redirect(request.url)
+        except:
+            return "There was an error updating the entry!"
+
+    form.name.data = entry.name
+    form.amount.data = entry.amount
+
+    return render_template('update.html',
+                            title='Update ' + entry_type.capitalize(),
+                            form=form,
+                            entry=entry)
+
+
+@app.route('/<entry_type>/delete/<int:id>')
+def delete_entry(entry_type, id):
+    if entry_type == 'income':
+        entry = models.Income.query.get_or_404(id)
+    elif entry_type == 'expenditure':
+        entry = models.Expenditure.query.get_or_404(id)
+    else:
+        abort(404)  # handle other cases as needed
+
+    try:
+        db.session.delete(entry)
+        db.session.commit()
+        flash('Successfully deleted entry!', 'success')
+        return redirect(url_for('add' + entry_type.capitalize()))
+    except:
+        return "There was an error deleting the entry!"
